841c815ed41846820b88444b9dac9d949e4b417b
BarunPrakash <brnprksh@gmail.com>
2019-09-20 07:27:28 +0530

    Derived class

---
+++ Salary.java
@@ -0,0 +1,30 @@
+/* File name : Salary.java */
+public class Salary extends test {
+   private double salary; // Annual salary
+   
+   public Salary(String name, String address, int number, double salary) {
+      super(name, address, number);
+      setSalary(salary);
+   }
+   
+   public void mailCheck() {
+      System.out.println("Within mailCheck of Salary class ");
+      System.out.println("Mailing check to " + getEmp_name()
+      + " with salary " + salary);
+   }
+   
+   public double getSalary() {
+      return salary;
+   }
+   
+   public void setSalary(double newSalary) {
+      if(newSalary >= 0.0) {
+         salary = newSalary;
+      }
+   }
+   
+   public double computePay() {
+      System.out.println("Computing salary pay for " + getEmp_name());
+      return salary/52;
+   }
+}
